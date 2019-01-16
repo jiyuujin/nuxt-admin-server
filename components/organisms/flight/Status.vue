5881982ea7dd1b19538e8a1ba49ace6e3085fbad
jiyuujin LAB <jiyuujinunite@gmail.com>
2019-01-16 18:45:10 +0900

    Fix flight chart created by vue-chartjs #1

--- components/organisms/flight/Status.vue
+++ components/organisms/flight/Status.vue
@@ -1,18 +1,31 @@
 <template>
-  <div>
+  <div
+    v-if="list"
+  >
     <h3>
       Flight
     </h3>
-    <!--
-    <Graph
-      :data="list"
+    <FlightChart
+      :chart-data="getChartDataset(list)"
     />
-    -->
   </div>
 </template>
 
 <script>
-import Graph from './Graph'
+import FlightChart from './FlightChart'
+
+const YEARS = [
+  2015,
+  2016,
+  2017,
+  2018,
+  2019
+];
+
+const LABEL_TEXT = 'Flights';
+
+const COLOR_TEXT = '#42b883';
+
 export default {
   props: {
     list: {
@@ -21,7 +34,31 @@ export default {
     }
   },
   components: {
-    Graph
+    FlightChart
+  },
+  methods: {
+    getChartDataset(items) {
+      let dataset = []
+      for (let yearIndex = 0; yearIndex < YEARS.length; yearIndex++) {
+        const size = items.filter(item => {
+          return item.data.time.includes(YEARS[yearIndex]) === true
+        }) || 0
+
+        // console.log(YEARS[yearIndex] + ' : ' + size.length)
+        dataset.push(size.length)
+      }
+
+      return {
+        labels: YEARS,
+        datasets: [
+          {
+            label: LABEL_TEXT,
+            backgroundColor: COLOR_TEXT,
+            data: dataset
+          }
+        ]
+      }
+    }
   }
 }
 </script>
