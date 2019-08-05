9375d24779b57e786ccbd95789bd06430e489bad
jiyuujin LAB <jiyuujinunite@gmail.com>
2019-08-06 06:50:30 +0900

    Unitテストを追加

---
+++ client/tests/unit/atoms/input.spec.ts
@@ -0,0 +1,16 @@
+import Child from '@/components/atoms/Input.vue'
+
+import { mount } from '@vue/test-utils'
+
+// @testing-library/vue
+import '@testing-library/jest-dom/extend-expect'
+import { cleanup } from '@testing-library/vue'
+
+afterEach(cleanup)
+
+describe('shallow sample', () => {
+    test('is a Vue instance', () => {
+        const wrapper = mount(Child)
+        expect(wrapper.isVueInstance()).toBeTruthy()
+    })
+})
