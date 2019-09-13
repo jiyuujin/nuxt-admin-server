26d34c8d6055675d4d1498b65f086219f34c64a1
yuma kitamura <yuma.kitamura@smaregi.jp>
2019-09-13 13:48:50 +0900

    css-loader、vue-loaderのインストール漏れ

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
-const HardSource = require('hard-source-webpack-plugin')
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin')
 
 module.exports = ({ config, mode }) => {
   config.module.rules.push(
@@ -46,11 +46,11 @@ module.exports = ({ config, mode }) => {
     })
   )
   config.plugins.push(
-    new HardSource()
+    new HardSourceWebpackPlugin()
   )
 
   config.resolve.alias['@'] = path.dirname(path.resolve(__dirname))
   config.resolve.alias['~'] = path.dirname(path.resolve(__dirname))
 
-  return config;
-};
+  return config
+}
