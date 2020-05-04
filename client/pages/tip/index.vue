c4ace2aa1b5ddd33330c04353a6d349db6cc97be
jiyuujin LAB <jiyuujinunite@gmail.com>
2020-05-04 14:31:37 +0900

    Lint fix

--- client/pages/tip/index.vue
+++ client/pages/tip/index.vue
@@ -109,7 +109,7 @@ export default defineComponent({
     const eventOptions = computed(() => {
       let array: object[] = []
       if (state.events.item !== undefined) {
-        state.events.item.forEach(item => {
+        state.events.item.forEach((item) => {
           array.push({
             value: item.data.id,
             text: item.data.name
@@ -148,7 +148,7 @@ export default defineComponent({
       },
       applyTags(value: number) {
         let isSame: boolean = false
-        state.form.tags.map(tag => {
+        state.form.tags.map((tag) => {
           if (tag === value) {
             isSame = true
           }
@@ -162,7 +162,7 @@ export default defineComponent({
       },
       tagText(tagId: number) {
         let result: string = ''
-        CATEGORIES.map(category => {
+        CATEGORIES.map((category) => {
           if (category.value === tagId) {
             result = category.text
           }
