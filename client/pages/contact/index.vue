aae99515a3bd5f4c114be31d30bbb904868795aa
jiyuujin <jiyuujinunite@gmail.com>
2020-12-06 16:42:24 +0900

    fetch meetups, modal usage for contact and flight

--- client/pages/contact/index.vue
+++ client/pages/contact/index.vue
@@ -1,5 +1,24 @@
 <template>
   <main-template :user-status="userStatus">
+    <modal title="問い合わせを確認する" @cancel="cancel">
+      <div :style="{ padding: '8px', height: '400px', overflowY: 'scroll' }">
+        <div v-for="item in contacts.item" :key="item.id">
+          <div v-if="item.page === state.activePage">
+            <div class="px-4 py-2">
+              {{ titleText(item) }}
+            </div>
+          </div>
+        </div>
+
+        <j-pagination
+          :items="contacts.item !== undefined ? contacts.item : []"
+          :page="state.activePage"
+          :per-page="state.perPage"
+          @handlePage="applyPage"
+        />
+      </div>
+    </modal>
+
     <div :style="{ display: 'flex', flexWrap: 'wrap', width: '100%' }">
       <google-chart
         chart-type="ColumnChart"
@@ -12,22 +31,51 @@
         }"
         style="width: 50%"
       />
+      <google-chart
+        chart-type="ColumnChart"
+        :chart-data="chartDataByVue"
+        :chart-options="{
+          width: 450,
+          height: 300,
+          legend: { position: 'top', maxLines: 3 },
+          bar: { groupWidth: '75%' }
+        }"
+        style="width: 50%"
+      />
+      <google-chart
+        chart-type="ColumnChart"
+        :chart-data="chartDataByTypeScript"
+        :chart-options="{
+          width: 450,
+          height: 300,
+          legend: { position: 'top', maxLines: 3 },
+          bar: { groupWidth: '75%' }
+        }"
+        style="width: 50%"
+      />
+      <google-chart
+        chart-type="ColumnChart"
+        :chart-data="chartDataByFJUGMeetup"
+        :chart-options="{
+          width: 450,
+          height: 300,
+          legend: { position: 'top', maxLines: 3 },
+          bar: { groupWidth: '75%' }
+        }"
+        style="width: 50%"
+      />
+      <google-chart
+        chart-type="ColumnChart"
+        :chart-data="chartDataByFJUGHandson"
+        :chart-options="{
+          width: 450,
+          height: 300,
+          legend: { position: 'top', maxLines: 3 },
+          bar: { groupWidth: '75%' }
+        }"
+        style="width: 50%"
+      />
     </div>
-
-    <div v-for="item in contacts.item" :key="item.id">
-      <div v-if="item.page === state.activePage">
-        <div class="px-4 py-2">
-          {{ titleText(item) }}
-        </div>
-      </div>
-    </div>
-
-    <j-pagination
-      :items="contacts.item !== undefined ? contacts.item : []"
-      :page="state.activePage"
-      :per-page="state.perPage"
-      @handlePage="applyPage"
-    />
   </main-template>
 </template>
 
@@ -37,15 +85,24 @@ import { defineComponent, SetupContext } from '@vue/composition-api'
 import UserComposable from '~/composables/user'
 import ContactComposable from '~/composables/contact'
 
-import { drawChart, fetchContacts } from '~/services/contactService'
+import {
+  drawChart as drawContactChart,
+  fetchContacts
+} from '~/services/contactService'
+import {
+  drawChart as drawMeetupChart,
+  fetchMeetups
+} from '~/services/meetupService'
 
 const MainTemplate = () => import('~/components/MainTemplate.vue')
 const GoogleChart = () => import('~/components/GoogleChart.vue')
+const Modal = () => import('~/components/Modal.vue')
 
 export default defineComponent({
   components: {
     MainTemplate,
-    GoogleChart
+    GoogleChart,
+    Modal
   },
   middleware: 'auth',
   setup(props: {}, ctx: SetupContext) {
@@ -55,9 +112,23 @@ export default defineComponent({
   },
   async asyncData() {
     const contacts = await fetchContacts()
+    const meetups = await fetchMeetups()
     return {
       contacts: contacts,
-      chartData: drawChart(contacts.item)
+      chartData: drawContactChart(contacts.item),
+      meetups: meetups,
+      chartDataByVue: drawMeetupChart(meetups, 'meetup', 'v-kansai'),
+      chartDataByTypeScript: drawMeetupChart(meetups, 'meetup', 'kansai.ts'),
+      chartDataByFJUGMeetup: drawMeetupChart(
+        meetups,
+        'meetup',
+        'Flutter Osaka'
+      ),
+      chartDataByFJUGHandson: drawMeetupChart(
+        meetups,
+        'handson',
+        'Flutter Osaka'
+      )
     }
   }
 })
